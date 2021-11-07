1ec9f857dc6063f45a0ef5c18c31689d2f908c70
Filip Felipe <filipfelipe@outlook.com>
2021-11-07 17:55:26 -0300

    Gerenciador

---
+++ Gerenciador/config/mqtt.js
@@ -0,0 +1,12 @@
+import mqtt from 'mqtt';
+
+var url = process.env.HOST_MQTT;
+var options = {
+  port: process.env.PORT_MQTT,
+  clientId: 'mqttjs_' + Math.random().toString(16).substr(3, 8),
+  username: process.env.USER_MQTT,
+  password: process.env.PASSWORD_MQTT,
+};
+var client = mqtt.connect(url, options);
+
+export default client;
